be9b22a24b366b7f18968ac042ee430e2a0a76c9
maksoon14 <sotovista1701gmail.com>
2020-09-18 21:46:46 +0300

    pract 1.1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    struct Person
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+
+        public Person(string _firstname, string _lastname, int _age)
+        {
+            
+            firstname = _firstname;
+            lastname = _lastname;
+            age = _age;
+        }
+
+        public override string ToString()
+        {
+            return firstname + " " + lastname + ", age " + age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p = new Person("Tony", "Allen", 32);
+            Console.WriteLine(p);
+        }
+    }
+}
