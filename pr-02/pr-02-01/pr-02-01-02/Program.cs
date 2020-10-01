28f273aa7891026a846a979a511fac15b3e16c8f
maksoon14 <sotovista1701gmail.com>
2020-10-01 19:44:06 +0300

    pr02-01-02

---
+++ pr-02/pr-02-01/pr-02-01-02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace pr_02_01_02
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //тк файлы .ini это фишка винды, а я сижу на линуксе, то данная программа будет мониторить изменения .txt файлов в главной директории
+            FileSystemWatcher watcher = new FileSystemWatcher("/home");
+            watcher.Filter = "*.txt";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+            watcher.EnableRaisingEvents = true;
+            Console.Read();
+        }
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+    }
+}
