e6e9d48de4a65d2444c1862c791e6e9d5fef39ea
maksoon14 <sotovista1701gmail.com>
2020-10-02 18:28:56 +0300

    pr-02-04-02

---
+++ pr-02/pr-02-04/pr-02-04-02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace pr_02_04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.Close();
+
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("File not found");
+            }
+            else
+            {
+                userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+                StreamReader userReader = new StreamReader(userStream);
+                string contents = userReader.ReadToEnd();
+                Console.WriteLine(contents);
+            }
+        }
+    }
+}
