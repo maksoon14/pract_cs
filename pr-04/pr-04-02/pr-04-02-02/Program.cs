8510e7c289ecb040ba8e4390eaeb2e96ac6c2d65
maksoon14 <sotovista1701gmail.com>
2020-10-03 03:13:41 +0300

    pr-04-02-02

---
+++ pr-04/pr-04-02/pr-04-02-02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace pr_04_02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQ = new Queue();
+            myQ.Enqueue("First");
+            myQ.Enqueue("Second");
+            myQ.Enqueue("Third");
+            myQ.Enqueue("Fourth");
+
+            while (myQ.Count > 0)
+            {
+                object obj = myQ.Dequeue();
+                Console.WriteLine("Frome myQ: {0}", obj);
+            }
+            
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+            }
+        }
+    }
+}
