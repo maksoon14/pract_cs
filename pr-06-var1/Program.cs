6251b7279fe25618e86aadae01805e23550652d5
maksoon14 <sotovista1701gmail.com>
2020-10-08 20:54:41 +0300

    pr-06

--- pr-06-var1/Program.cs
+++ pr-06-var1/Program.cs
@@ -25,7 +25,53 @@ namespace pr_06_var1
             student1.AddExams(AnotherExams);
             Console.WriteLine(student1);
             
+            int nRow, nColumn;
+            Console.WriteLine("Ввдете два числа через пробел или запятую: ");
+            var size = Console.ReadLine().Split(' ', ',');
+            nRow = int.Parse(size[0]);
+            nColumn = int.Parse(size[1]);
+
+            var examsLinear = new Exam[nRow * nColumn];
+            for(int i = 0; i < nRow * nColumn; i++)
+                examsLinear[i] = new Exam();
             
+            var examsDouble = new Exam[nRow, nColumn];
+            for(var i = 0; i < nRow; i++)
+            for(var j = 0; j < nColumn; j++)
+                examsDouble[i,j] = new Exam();
+
+            var examsJagged = new Exam[nRow][];
+            for (var i = 0; i < nRow; i++)
+            {
+                examsJagged[i] = new Exam[nColumn];
+                for (var j = 0; j < nColumn; j++)
+                    examsJagged[i][j] = new Exam();
+            }
+
+            var t1 = Environment.TickCount;
+            for (int i = 0; i < nRow * nColumn; i++)
+                examsLinear[i].ExamMark = 5;
+            var t2 = Environment.TickCount;
+            var t = t2 - t1;
+            Console.WriteLine("nrow: {0}, ncolumn: {1}\n", nRow, nColumn);
+            Console.WriteLine("Одномерный массив: {0}", t);
+
+            t1 = Environment.TickCount;
+            for(var i = 0; i < nRow; i++)
+            for(var j = 0; j < nColumn; j++)
+                examsDouble[i,j].ExamMark = 5;
+            t2 = Environment.TickCount;
+            t = t2 - t1;
+            Console.WriteLine("Двумерный прямоугольный массив: {0}", t);
+
+            
+            t1 = Environment.TickCount;
+            for(var i = 0; i < nRow; i++)
+            for (var j = 0; j < nColumn; j++)
+                examsJagged[i][j].ExamMark = 5;
+            t2 = Environment.TickCount;
+            t = t2 - t1;
+            Console.WriteLine("Двумерный ступенчатый массив: {0}", t);
         }
     }
 }
